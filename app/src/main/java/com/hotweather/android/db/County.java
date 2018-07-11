a78ecb491a8ac2d03969e398eb73609e4d01f5ec
SirGitRight <wanghuisong123@126.com>
2018-07-12 00:13:31 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/hotweather/android/db/County.java
@@ -0,0 +1,41 @@
+package com.hotweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by PKUCCERZJ on 2018/7/12.
+ */
+
+public class County extends DataSupport {
+
+    private int id;
+
+    private String countyName;
+
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+}
